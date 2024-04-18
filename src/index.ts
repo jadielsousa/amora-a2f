48b0d5e9f029cb5f3dc9f95b634cfe118728e202
jadielsousa <jadielsn@gmail.com>
2024-04-18 13:31:25 -0400

    Primeiro envio

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import notp from "notp";
+import crypto from "crypto";
+import b32 from "thirty-two";
+import { Options } from "./interface";
+import { toDataURL } from "qrcode";
+
+async function teste  (dados: string) {
+  const retorno = await toDataURL(dados);
+  console.log(dados, "otp token qr: ", retorno);
+  return retorno;
+}
+
+//qr: `https://chart.googleapis.com/chart?chs=166x166&chld=L|0&cht=qr&chl=${uri}${encodedQuery}`,
+export function generateSecret(options?: Options) {
+  const config = {
+    name: encodeURIComponent(options?.name ?? "App"),
+    account: encodeURIComponent(options?.account ? `:${options.account}` : ""),
+  } as const;
+
+  const bin = crypto.randomBytes(20);
+  const base32 = b32.encode(bin).toString("utf8").replace(/=/g, "");
+
+  const secret = base32
+    .toLowerCase()
+    .replace(/(\w{4})/g, "$1 ")
+    .trim()
+    .split(" ")
+    .join("")
+    .toUpperCase();
+
+  const query = `?secret=${secret}&issuer=${config.name}`
+  const encodedQuery = query.replace('?', '%3F').replace('&', '%26')
+  const uri = `otpauth://totp/${config.name}${config.account}`
+  const qrcode = teste(`${uri}${query}`)
+  return {
+    secret,
+    uri: `${uri}${query}`,
+  qr: `${qrcode}`
+  };
+}
+
+export function generateToken(secret: string) {
+  if (!secret || !secret.length) return null;
+  const unformatted = secret.replace(/\W+/g, "").toUpperCase();
+  const bin = b32.decode(unformatted);
+
+  return { token: notp.totp.gen(bin) };
+}
+
+export function verifyToken(secret: string, token?: string, window = 4) {
+  if (!token || !token.length) return null;
+
+  const unformatted = secret.replace(/\W+/g, "").toUpperCase();
+  const bin = b32.decode(unformatted);
+
+  return notp.totp.verify(token.replace(/\W+/g, ""), bin, {
+    window,
+    time: 30,
+  });
+}
